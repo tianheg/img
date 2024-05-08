95d1c2f9cc29e036396d433e3c72c324a4515419
tianheg <i@tianheg.co>
2024-05-08 15:31:57 +0800

    change adapter to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import auth from 'auth-astro';
-import vercel from '@astrojs/vercel/serverless';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [auth(), tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
