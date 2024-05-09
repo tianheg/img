b1c35f6106e89305109ce0f7a314c068a715f5cb
tianheg <i@tianheg.co>
2024-05-09 15:40:42 +0800

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
+	adapter: cloudflare({ mode: "directory" }),
 });
