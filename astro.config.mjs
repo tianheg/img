7e2a0387ad74bed4fdf2d217e7c3ee577ad83456
tianheg <i@tianheg.co>
2024-05-09 16:33:38 +0800

    rm cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-	adapter: cloudflare({ mode: "directory" }),
 });
