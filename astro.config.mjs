0709d27baf66994c04f8d9f1c2e2023040f8991e
tianheg <i@tianheg.co>
2025-04-29 22:55:58 +0800

    upgrade daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
